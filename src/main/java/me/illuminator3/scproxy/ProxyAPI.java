205501c5b04d9d352c140853e32f7f28d23ddd7d
illuminator3 <48388251+illuminator3@users.noreply.github.com>
2020-06-30 09:51:52 +0200

    Set timeout to 1000

--- src/main/java/me/illuminator3/scproxy/ProxyAPI.java
+++ src/main/java/me/illuminator3/scproxy/ProxyAPI.java
@@ -2,9 +2,9 @@ package me.illuminator3.scproxy;
 
 public enum ProxyAPI
 {
-    PROXYSCRAPE_HTTP("https://api.proxyscrape.com/?request=displayproxies&proxytype=http&timeout=5000&anonymity=elite&ssl=yes", ConnectionType.HTTP),
-    PROXYSCRAPE_SOCKS4("https://api.proxyscrape.com/?request=displayproxies&proxytype=socks4&timeout=5000&anonymity=elite&ssl=yes", ConnectionType.SOCKS4),
-    PROXYSCRAPE_SOCKS5("https://api.proxyscrape.com/?request=displayproxies&proxytype=socks5&timeout=5000&anonymity=elite&ssl=yes", ConnectionType.SOCKS5);
+    PROXYSCRAPE_HTTP("https://api.proxyscrape.com/?request=displayproxies&proxytype=http&timeout=1000&anonymity=elite&ssl=yes", ConnectionType.HTTP),
+    PROXYSCRAPE_SOCKS4("https://api.proxyscrape.com/?request=displayproxies&proxytype=socks4&timeout=1000&anonymity=elite&ssl=yes", ConnectionType.SOCKS4),
+    PROXYSCRAPE_SOCKS5("https://api.proxyscrape.com/?request=displayproxies&proxytype=socks5&timeout=1000&anonymity=elite&ssl=yes", ConnectionType.SOCKS5);
 
     private final String requestURL;
     private final ConnectionType type;
